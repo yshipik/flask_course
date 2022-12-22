9e4c55d79a2915028fab9bb1337f2374b78e2f55
Юлиан Шипик <yshipik@yandex.ru>
2022-12-23 01:01:47 +0300

    финальная версия

--- database.py
+++ database.py
@@ -10,29 +10,42 @@ class Database:
     __PAGE_SIZE = 15
 
     def __init__(self, file=""):
-        '''Конструктор класса, создаем соединение и курсор базы данных'''
+        # конструктор класса Базы данных
         if not file:
+            # создание соединения с бд
             self._conn = sqlite3.connect(app.config['DATABASE'])
         else:
             self._conn = sqlite3.connect(file)
         self._conn.row_factory = sqlite3.Row
+        # создание курсора базы данных
         self._cursor = self._conn.cursor()
 
-    def add_menu(self, name: str, url: str, category: str):
-        '''Добавление пункта в меню, используя информацию полученную из форм'''
-        try:
-            self._cursor.execute("insert into menu values (NULL, ?, ?, ?)", (name, url, category))
-            self._conn.commit()
-        except sqlite3.Error as e:
-            print("Ошибка добавления в меню БД" + str(e))
-            return False
-        return True
+    def state_db_function(errormessage):
+        # декоратор для функций, которые не возвращают результаты из бд
+        def wrap(fn):
+            def wrapper(*args, **kwargs):
+                try:
+                    fn(*args, **kwargs)
+                    return True
+                except sqlite3.Error as e:
+                    print(errormessage, str(e))
+                return False
+            return wrapper
 
+        return wrap
+    @state_db_function("Ошибка при добавлении пункта в меню")
+    def add_menu(self, name: str, url: str, category: str):
+        # добавление пункта в меню
+        self._cursor.execute("insert into menu values (NULL, ?, ?, ?)", (name, url, category))
+        self._conn.commit()
     def add_user(self, username: str, password: str, is_stuff=False):
-        '''Добавление пользователя'''
+        # получение текущего времени
         tm = datetime.datetime.now()
+        # получение хэша пароля
+        realpass = sha256(bytes(password, encoding="utf-8")).hexdigest()
         try:
-            self._cursor.execute("insert into users values (NULL, ?, ?, ?, ?)", (username, password, tm, is_stuff))
+            # добавление пользователя
+            self._cursor.execute("insert into users values (NULL, ?, ?, ?, ?)", (username, realpass, tm, is_stuff))
             self._conn.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления пользователя в БД: " + str(e))
@@ -51,8 +64,9 @@ class Database:
 
     def login(self, username: str, password: str):
         '''Проверяет, существует ли пользователь с таким именем и паролем'''
+        realpass = sha256(bytes(password, encoding="utf-8")).hexdigest()
         try:
-            self._cursor.execute("select * from users where username = ? and password = ?", (username, password))
+            self._cursor.execute("select * from users where username = ? and password = ?", (username, realpass))
             res = self._cursor.fetchone()
             if res: return True
         except sqlite3.Error as e:
@@ -123,18 +137,13 @@ class Database:
         except sqlite3.Error as e:
             print("Произошла ошибка при верификации пользователя" + str(e))
         return False
-
+    @state_db_function("Ошибка при добавлении предмета в базу данных")
     def add_stuff(self, title: str, description: str, picture: str, price: int):
         '''Добавление предмета в базу данных'''
         tm = datetime.datetime.now()
-        try:
-            self._cursor.execute("insert into stuff values(NULL, ?, ?, ?, ?,?)",
-                                 (title, description, price, tm, picture))
-            self._conn.commit()
-            return True
-        except sqlite3.Error as e:
-            print("Произошла ошибка при добавлении поста" + str(e))
-        return False
+        self._cursor.execute("insert into stuff values(NULL, ?, ?, ?, ?,?)",
+                            (title, description, price, tm, picture))
+        self._conn.commit()
 
     def get_stuff(self, page: int):
         '''Получение предметов из базы данных'''
@@ -147,22 +156,17 @@ class Database:
         except sqlite3.Error as e:
             print("Произошла ошибка при попытке получить товары из магазина " + str(e))
         return False
-
+    @state_db_function("Произошла ошибка при добавлении покупки")
     def add_purchase(self, username: str, itemid: int):
         '''Добавление покупки'''
         tm = datetime.datetime.now()
-        try:
-            self._cursor.execute("select * from users where username = ?", (username,))
-            res = self._cursor.fetchone()
-            if res:
-                user_id = res["id"]
-                self._cursor.execute("insert into purchases values(NULL, ?, ?, ?, ?, NULL)",
-                                     (tm, itemid, user_id, "await",))
-                self._conn.commit()
-                return True
-        except sqlite3.Error as e:
-            print("Произошла ошибка во время покупки товара")
-        return False
+        self._cursor.execute("select * from users where username = ?", (username,))
+        res = self._cursor.fetchone()
+        if res:
+            user_id = res["id"]
+            self._cursor.execute("insert into purchases values(NULL, ?, ?, ?, ?, NULL)",
+                                (tm, itemid, user_id, "await",))
+            self._conn.commit()
 
     def get_bucket(self, username: str):
         '''Получение товаров добавленных в корзину и заказанных пользователем'''
@@ -172,7 +176,8 @@ class Database:
             if res:
                 user_id = res["id"]
                 self._cursor.execute(
-                    "select purchases.id as mainid, * from purchases, stuff where purchases.itemid = stuff.id and purchases.userid = ?",
+                    "select purchases.id as mainid, * from purchases, stuff "
+                    "where purchases.itemid = stuff.id and purchases.userid = ? and purchases.state = 'await'",
                     (user_id,))
                 res = self._cursor.fetchall()
                 return res
@@ -180,33 +185,27 @@ class Database:
             print("Произошла ошибка во время покупки товара" + str(e))
         return []
 
+    @state_db_function("Ошибка при удалении элемента из корзины")
     def safe_delete_from_bucket(self, username, id):
         '''Удаление элемента из корзины'''
-        try:
-            self._cursor.execute("select * from users where username = ?", (username,))
-            res = self._cursor.fetchone()
-            if res:
-                user_id = res["id"]
-                self._cursor.execute("delete from purchases where id = ? and userid = ?", (id, user_id))
-                self._conn.commit()
-        except sqlite3.Error as e:
-            print("Ошибка при удалении элемента ", e)
-
+        self._cursor.execute("select * from users where username = ?", (username,))
+        res = self._cursor.fetchone()
+        if res:
+            user_id = res["id"]
+            self._cursor.execute("delete from purchases where id = ? and userid = ?", (id, user_id))
+            self._conn.commit()
+    @state_db_function("Ошибка при добавлении жалобы")
     def add_complaint(self, title: str, content: str, username: str):
         '''Добавление жалобы'''
         tm = datetime.datetime.now()
-        try:
-            self._cursor.execute("select * from users where username = ?", (username,))
-            res = self._cursor.fetchone()
-            if res:
-                user_id = res["id"]
-                self._cursor.execute("insert into complaints values(NULL, ?, ?, ?, ?, ?)",
-                                     (title, content, tm, user_id, "await"))
-                self._conn.commit()
-                return True
-        except sqlite3.Error as e:
-            print("Произошла ошибка во время покупки товара")
-        return False
+
+        self._cursor.execute("select * from users where username = ?", (username,))
+        res = self._cursor.fetchone()
+        if res:
+            user_id = res["id"]
+            self._cursor.execute("insert into complaints values(NULL, ?, ?, ?, ?, ?)",
+                                (title, content, tm, user_id, "await"))
+            self._conn.commit()
 
     def get_complaints(self):
         '''Получение жалоб из Базы данных'''
@@ -227,58 +226,90 @@ class Database:
             if res: return res
         except sqlite3.Error as e:
             print("Не удается получить дату:" + str(e))
-        return
-
+        return []
+    @state_db_function("Ошибка при удалении заказа")
     def add_order(self, location: str):
-        '''Функция для добавления заказа в список заказов'''
         tm = datetime.datetime.now()
+        # добавление заказа в базу данных
+        self._cursor.execute("insert into orders values (NULL, ?, 'await', ?, NULL)", (tm, location))
+        self._conn.commit()
+    def get_user_id(self, username: str):
+        # получение id пользователя
         try:
-            self._cursor.execute("insert into orders values (NULL, ?, 'await', ?)", (tm, location))
-            self._conn.commit()
-            return True
+            self._cursor.execute("select id from users where username = ?", (username,))
+            res = self._cursor.fetchone()
+            if res:
+                return res["id"]
         except sqlite3.Error as e:
-            print("Не удалось добавить заказ")
-        return False
-
+            print("Не удалось получить id пользователя")
+    @state_db_function("Ошибка при добавлении функции")
     def send_to_order(self, username: str, location: str):
-        '''Перемещает все товары из корзины и создает новый заказ'''
-        tm = datetime.datetime.now()
-        try:
-            self._cursor.execute("insert into orders values (NULL, ?, 'await', ?)", (tm, location))
+        # получение пользователя
+        self._cursor.execute("select * from users where username = ?", (username,))
+        user = self._cursor.fetchone()
+        self._cursor.execute("select count(*) as times from purchases where userid = ? and state = 'await'", (user["id"],))
+        # считаем количество заказов
+        res = self._cursor.fetchone()
+        if res["times"] != 0:
+
+            tm = datetime.datetime.now()
+            self._cursor.execute("insert into orders values (NULL, ?, 'await', ?, NULL)", (tm, location))
             # изменяем те заказы, которые находятся в магазине
             self._conn.commit()
             self._cursor.execute("select * from orders where timestamp = ?", (tm,))
-
             order = self._cursor.fetchone()
             if order:
-                # id пользователя
-                self._cursor.execute("select * from users where username = ?", (username,))
-                user = self._cursor.fetchone()
                 # даем этим заказам id пользователя
                 if user:
-                    print(order["id"], user["id"])
                     self._cursor.execute("update purchases set orderid = ?, state = ? where userid = ? and state = ?",
-                                         (order["id"], "done", user["id"], "await"))
+                                        (order["id"], "done", user["id"], "await"))
                     self._conn.commit()
-        except sqlite3.Error as e:
-            print("Ошибка при добавлении заказа: " + str(e))
+            return True
+        return False
+    def get_all_orders(self):
+        try:
+            # получаем все заказы из базы данных
+            self._cursor.execute('select orderid, status, price, title, time, '
+                                 'description, timestamp, workstamp from purchases, orders, stuff '
+                                 'where (orderid >= 0 and orderid = orders.id and itemid = stuff.id) '
+                                 'group by purchases.id')
+            result = self._cursor.fetchall()
 
+            print(result)
+            # возвращаем из функции
+            if result:
+                return result
+        except sqlite3.Error as e:
+            print("Ошибка при получении всех заказов " + str(e))
+        return []
+    def get_orders(self, username: str):
+        try:
+            id_ = self.get_user_id(username)
+            # получаем заказы для определенного пользователя
+            self._cursor.execute('select orderid, status, price, title, time, description, timestamp, workstamp '
+                                 'from purchases, orders, stuff'
+                                 ' where (orderid >= 0 and orderid = orders.id and userid = ? and itemid = stuff.id) '
+                                 'group by purchases.id', (id_,))
+            result = self._cursor.fetchall()
+            print(result)
+            if result:
+                return result
+        except sqlite3.Error as e:
+            print("Ошибка при получении заказа: " + str(e))
+        return []
+    @state_db_function("Ошибка при удалении отзываы")
     def remove_complaint(self, id: int):
-        '''Удаление отзыва'''
-        try:
-            self._cursor.execute("delete from complaints where id = ?", (id,))
-            self._conn.commit()
-            return True
-        except sqlite3.Error as e:
-            print("Ошибка при удалении заказа" + str(e))
-        return False
-
+        # удаление жалобы, и обновление бд
+        self._cursor.execute("delete from complaints where id = ?", (id,))
+        self._conn.commit()
+    @state_db_function("Ошибка при удалении удалении товара из магазина")
     def remove_stuff(self, id: int):
-        '''Удаление товара из магазина'''
-        try:
-            self._cursor.execute("delete from stuff where id = ?", (id,))
-            self._conn.commit()
-            return True
-        except sqlite3.Error as e:
-            print("Ошибка при удалении заказа" + str(e))
-        return False
+        # удаление товара, и обновление бд
+        self._cursor.execute("delete from stuff where id = ?", (id,))
+        self._conn.commit()
+    @state_db_function("Ошибка при изменении статуса заказа")
+    def change_order_state(self, id: int, status: str):
+        # изменение статуса заказа  и обновление бд
+        tm = datetime.datetime.now()
+        self._cursor.execute("update orders set status = ?, workstamp = ? where id = ?", (status, tm, id))
+        self._conn.commit()
--- routes.py
+++ routes.py
@@ -10,7 +10,6 @@ from werkzeug.utils import secure_filename
 
 EXTENSION = {'png', 'jpg', 'jpeg', 'bmp'}
 
-
 def save_tuple(obj, key):
     if key in obj:
         return obj[key]
@@ -34,11 +33,16 @@ def index():
 def account():
     db = Database()
     if "user" in session:
+        # если пользователь зарегистрирован
         if db.verify_user(session["user"], session["session_key"]):
+            # проверяем данные сессии
             if request.method == "POST":
+                # обработка post запросов
                 if "id" in request.form:
+                    # удаление элемента из корзины
                     db.safe_delete_from_bucket(session["user"], request.form['id'])
                 elif "address" in request.form:
+                    # оформление заказа
                     db.send_to_order(session["user"], request.form["address"])
 
             # все нормально
@@ -47,19 +51,36 @@ def account():
             return render_template("account.html", username=session["user"], menu_data=db.get_menu(),
                                    date=db.get_registration_time(session["user"])["register_date"],
                                    menu_category=save_tuple(session, "group"), group=yourgroup, count_items=len(items),
-                                   bucket=items)
-        abort(401)
+                                   bucket=items, orders=db.get_orders(session["user"]), title="Аккаунт")
+    abort(401)
 
+@app.route("/orders", methods=["GET", "POST"])
+def orders():
+    db = Database()
+    if db.verify_admin(session["user"], session["session_key"]):
+        # проверяем, что это аккаунт администратора
+        if request.method == "POST":
+
+            if "id_confirm" in request.form:
+                db.change_order_state(request.form["id_confirm"], "done")
+            elif "id_cancel" in request.form:
+                db.change_order_state(request.form["id_cancel"], "working")
+        return render_template("orders.html", menu_data = db.get_menu(), menu_category=save_tuple(session, "group"), orders=db.get_all_orders(), title="Заказы")
+    abort(401)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     db = Database()
     if request.method == "POST":
         if db.register(request.form["username"]):
+            # регистрация пользователя
             res = db.add_user(request.form["username"], request.form["password"])
+            # изменение сессии пользователя
             session["user"] = request.form["username"]
             session["session_key"] = sha256(
-                bytes(request.form["username"] + request.form["password"], encoding="utf-8")).hexdigest()
+                bytes(request.form["username"] +
+                      sha256(bytes(request.form["password"], encoding="utf-8")).hexdigest(),
+                      encoding="utf-8")).hexdigest()
             session["group"] = db.get_group(request.form["username"])
             if not res:
                 flash("Возникла непредвиденная ошибка", category="error")
@@ -95,6 +116,7 @@ def shop():
     db = Database()
     if request.method == "POST" and "user" in session:
         if "item-id" in request.form:
+            # обработка добавления товара из магазина в корзину
             if db.verify_user(session["user"], session["session_key"]):
                 res = db.add_purchase(session["user"], request.form["item-id"])
                 if res:
@@ -103,6 +125,7 @@ def shop():
                 else:
                     flash("Возникла ошибка", category="error")
         elif "delete-id" in request.form:
+            # удаление товара, доступно только администраторам.
             res = db.remove_stuff(request.form["delete-id"])
             if res:
                 flash("Товар удален. Удачи в администрировании")
@@ -136,8 +159,10 @@ def complaints():
 @app.route("/view_complaints", methods=["GET", "POST"])
 def view():
     db = Database()
+    # просмотр жалоб
     if "user" in session and request.method == "POST":
         if db.verify_admin(session["user"], session["session_key"]):
+            # фукнция администратора, возможность удалить жалобы
             db.remove_complaint(request.form["id"])
 
     return render_template("view.html", complaints=db.get_complaints(), title="Отзывы", menu_data=db.get_menu(),
@@ -150,23 +175,25 @@ def create():
     if "user" in session:
         if db.verify_admin(session["user"], session["session_key"]):
             if request.method == "POST":
-                # проверка на плохие вещи
+                # проверка на правильную длину полей формы
                 if len(request.form["title"]) > 3 and len(request.form["description"]) > 5:
-                    print(request.files)
                     if "file" in request.files:
-                        print("file herer")
+                        # если файл был прикреплен
                         f = request.files['file']
                         if f.filename.split(".")[-1] in EXTENSION and "image" in f.mimetype:
+                            # создаем уникальное имя файла
                             filename = str(time.time()).replace(".", "_") + "." + f.filename.split(".")[-1]
-                            print(os.path.join(app.config["DOWNLOADS"], filename))
-                            f.save(os.path.join(app.config["DOWNLOADS"], filename))
-
+                            # сохранение файла
+                            f.save(app.root_path + url_for("get_image", filename=filename))
+                            # добавление товара в базу данных
                             res = db.add_stuff(request.form["title"], request.form["description"], filename,
                                                request.form["price"])
                             if not res:
                                 flash("Возникла ошибка при обработке вашего запроса на сервере", category="error")
                             else:
                                 flash("Успех", category="success")
+                        else:
+                            flash("Ошибка, расширение не подходит", category="success")
                 else:
                     flash("Слишком короткий заголовок или описание")
             return render_template("create.html", title="Новый товар", menu_data=db.get_menu(),
@@ -183,6 +210,7 @@ def logout():
 
 @app.route("/downloads/<filename>")
 def get_file(filename):
+    # получение файла из директории
     return send_from_directory(app.config["DOWNLOADS"], filename)
 
 
